0e99f0162ae83ba2b1b8963be2e72c8b8e8e3d90
MoRsHeD <mdmorshed0187@gmail.com>
2025-06-20 00:13:24 +0600

    added zod validation and created schema

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,18 +1,41 @@
 import express, { Request, Response } from 'express';
 import User from '../models/user.model';
+import { z } from 'zod';
 
 export const userRoutes = express.Router();
 
-userRoutes.post("/create-user", async (req : Request, res : Response) => {
-    const body = req.body;
+const CreateUserZodSchema = z.object(
+    {
+        firstName: z.string(),
+        lastName: z.string(),
+        age: z.number(),
+        email: z.string(),
+        password: z.string(),
+        role: z.string().optional()
+    }
+);
 
-    const user = await User.create(body);
+userRoutes.post("/create-user", async (req: Request, res: Response) => {
+    try {
+        // const body = req.body;
 
-    res.status(201).json({
-        success: true,
-        message: "User created successfully",
-        user
-    });
+        const body = await CreateUserZodSchema.parse(req.body);
+        console.log(body);
+
+        // const user = await User.create(body);
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            // user
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            success: false,
+            message: error.message,
+            error
+        })
+    }
 
 })
 
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,11 +1,12 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import validator from 'validator';
 
 
 const userSchema = new Schema<IUser>({
     firstName: {
         type: String,
-        required: true,
+        required: [true, 'First name is required'],
         minlength: [3, 'First name must be at least 3 characters, got {VALUE}'],
         maxlength: 15,
         trim: true
@@ -18,13 +19,24 @@ const userSchema = new Schema<IUser>({
     age: {
         type: Number,
         required: true,
-        min: [18, 'Age must be at least 18, got {VALUE}']
+        min: [18, 'Age must be at least 18, got {VALUE}'],
     },
     email: {
         type: String,
+        unique: [true, 'Email is already exist'],
         lowercase: true,
         required: true,
-        trim: true
+        trim: true,
+        validate : [validator.isEmail, "{VALUE} is not a valid email"]
+        // validate: {
+        //     validator: function (value) {
+        //         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+        //     },
+        //     message: props => `${props.value} is not a valid email`
+        //     // message : function(props){
+        //     //     return `${props.value} is not a validaa email`
+        //     // }
+        // }
     },
     password: {
         type: String,
@@ -33,7 +45,10 @@ const userSchema = new Schema<IUser>({
     role: {
         type: String,
         uppercase: true,
-        enum: ["USER", "ADMIN", "SUPERADMIN"],
+        enum: {
+            values: ["USER", "ADMIN", "SUPERADMIN"],
+            message: "Role is not valid, get {VALUE}"
+        },
         default: "USER"
     }
 });
